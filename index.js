f190c4389c13ae2205edd0bbe970d88077d95b2d
GongXian Cao <gongxiancao@hotmail.com>
2016-02-18 10:38:29 +0800

    add framework init code

---
+++ index.js
@@ -0,0 +1,22 @@
+var async = require('async');
+
+function Framework () {
+  this.middlewares = [];
+}
+
+Framework.prototype.use = function (middleware) {
+  this.middlewares.push(middleware);
+  return this;
+};
+
+Framework.prototype.lift = function (done) {
+  var self = this;
+  async.eachSeries(this.middlewares, function (middleware, done) {
+    middleware.call(self, done);
+  }, done);
+  return this;
+};
+
+module.exports = function () {
+  return global.framework = new Framework();
+};
