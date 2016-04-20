a1cd2fdaf33b4439d91b39bafd8a854c265831b3
GongXian Cao <gongxiancao@hotmail.com>
2016-04-20 19:04:57 +0800

    use promise and support lifted, and listened event

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var async = require('async');
 var EventEmitter = require('events');
 var util = require('util');
+var Promise = require('bluebird');
 var _ = require('lodash');
 
 function Framework () {
   this.middlewares = [];
+  this.promise = Promise.resolve();
   EventEmitter.call(this);
 }
 
@@ -20,49 +22,71 @@ Framework.prototype.use = function (middleware) {
   return this;
 };
 
-Framework.prototype.lift = function (done) {
+Framework.prototype.lift = function () {
+  this.promise = this.promise.then(this._lift.bind(this));
+  return this;
+};
+
+Framework.prototype._lift = function () {
   var self = this;
-  async.eachSeries(this.middlewares, function (middleware, done) {
+  return Promise.each(this.middlewares, function (middleware) {
     if(_.isFunction(middleware)){
-      return middleware.call(self, done);
+      return middleware.call(self);
     }
     if (_.isFunction(middleware.lift)){
-      return middleware.lift.call(self, done);
+      return middleware.lift.call(self);
     }
-    done();
-  }, function (err) {
-    if(err) {
-      self.emit('error', err);
-      return done(err);
-    }
+  })
+  .then(function () {
     self.emit('lifted');
-    done();
+  })
+  .catch(function (err) {
+    self.emit('error', err);
+    return Promise.reject(err);
   });
+};
+
+Framework.prototype.listen = function () {
+  this.promise = this.promise.then(this._listen.bind(this));
   return this;
 };
 
-Framework.prototype.lower = function (done) {
+Framework.prototype._listen = function () {
   var self = this;
-  async.eachSeries(this.middlewares.reverse(), function (middleware, done) {
-    if(_.isFunction(middleware.lower)) {
-      middleware.lower.call(self, function (err) {
-        if(err) {
-          self.emit('error', err);
-        }
-        done();
-      });
-    } else {
-      done();
+  return Promise.each(this.middlewares, function (middleware) {
+    if (_.isFunction(middleware.listen)){
+      return middleware.listen.call(self);
     }
-  }, function () {
-    self.emit('lowered');
-    if(done) {
-      done();
-    }
+  })
+  .then(function () {
+    self.emit('listened');
+  })
+  .catch(function (err) {
+    self.emit('error', err);
+    return Promise.reject(err);
   });
+};
+
+Framework.prototype.lower = function () {
+  this.promise = this.promise.then(this._lower.bind(this));
   return this;
 };
 
+Framework.prototype._lower = function () {
+  var self = this;
+  return Promise.each(this.middlewares, function (middleware) {
+    if (_.isFunction(middleware.lower)){
+      return middleware.lower.call(self);
+    }
+  })
+  .then(function () {
+    self.emit('lowered');
+  })
+  .catch(function (err) {
+    self.emit('error', err);
+    return Promise.reject(err);
+  });
+};
 
 module.exports = function () {
   return global.framework = new Framework();
